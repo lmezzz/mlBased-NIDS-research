fda586674f216e7d195d74f0c1aa82912e1f9801
lmezzz <ayaanra261@gmail.com>
2026-05-03 03:16:45 +0500

    feature imp saved

--- src/feature_importance.py
+++ src/feature_importance.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from config import MODELS_DIR
+from config import MODELS_DIR, RESULTS_DIR
 def analyse_lr(exp_name):
     saved = joblib.load(MODELS_DIR / f"lr_{exp_name}.pkl")
     model    = saved["model"]
@@ -17,6 +17,13 @@ def analyse_lr(exp_name):
     
     print(f"\n[LR] Feature importance — {exp_name.upper()}")
     print(coef_df.to_string(index=False))
+
+    coef_df.to_csv(
+        RESULTS_DIR / f"lr_importance_{exp_name}.csv",
+        index=False
+    )
+    print(f"[LR] Importance saved → lr_importance_{exp_name}.csv")
+
     return coef_df
 
 
@@ -32,6 +39,12 @@ def analyse_rf(exp_name):
     
     print(f"\n[RF] Feature importance — {exp_name.upper()}")
     print(imp_df.to_string(index=False))
+
+    imp_df.to_csv(
+        RESULTS_DIR / f"rf_importance_{exp_name}.csv",
+        index=False
+    )
+    print(f"[RF] Importance saved → rf_importance_{exp_name}.csv")
     return imp_df
 
 
@@ -47,6 +60,11 @@ def analyse_svm(exp_name):
     
     print(f"\n[SVM] Feature importance — {exp_name.upper()}")
     print(coef_df.to_string(index=False))
+    coef_df.to_csv(
+        RESULTS_DIR / f"svm_importance_{exp_name}.csv",
+        index=False
+    )
+    print(f"[SVM] Importance saved → svm_importance_{exp_name}.csv")
     return coef_df
 
 
